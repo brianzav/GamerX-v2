98fa91066bc7f8bfdbf40b601b14efb88c10be4e
Brian Zavala <brianzav23@gmail.com>
2024-03-22 06:19:58 -0300

    adding stockist controller

---
+++ src/main/java/br/com/senac/gamerx/controller/StockistController.java
@@ -0,0 +1,9 @@
+package br.com.senac.gamerx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/stockist")
+public class StockistController {
+}
