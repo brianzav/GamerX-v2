e9dc9fec577159dd7d914dc676d0e6f5227db8ba
brian.bzculque <brianzav23@gmail.com>
2024-04-01 18:06:17 -0300

    Adding product view for client

--- src/main/java/br/com/senac/gamerx/controller/ClientController.java
+++ src/main/java/br/com/senac/gamerx/controller/ClientController.java
@@ -1,11 +1,13 @@
 package br.com.senac.gamerx.controller;
 
 import br.com.senac.gamerx.dto.ProductDTO;
+import br.com.senac.gamerx.model.ProductModel;
 import br.com.senac.gamerx.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -33,5 +35,14 @@ public class ClientController {
                 .collect(Collectors.toList());
         model.addAttribute("products", productDTOs);
         return "listProductsClient";
+    }
+
+    @GetMapping("/products/view/{id}")
+    public String viewProduct(@PathVariable Long id, Model model) {
+        ProductModel product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Produto não encontrado."));
+        model.addAttribute("product", product);
+        model.addAttribute("images", product.getProductImages());
+        return "telaProdClient";
     }
 }
