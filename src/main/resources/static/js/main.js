c0c9c408a7f372e7a1c7bf58b3b84c82ded7d5f9
Tarsper <rafaelsilva106@outlook.com>
2024-04-02 16:57:25 -0300

    SLIDE COM CARDS

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,14 @@
+let swiperCards = new Swiper('.teste', {
+
+    loop: true,
+  
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
