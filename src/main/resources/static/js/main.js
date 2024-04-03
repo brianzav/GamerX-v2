f13902c69e5d2dc8bde1f13485577f42025eeefe
Tarsper <rafaelsilva106@outlook.com>
2024-04-03 13:04:01 -0300

    SLIDE PRODUTOS

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,14 +1,26 @@
-let swiperCards = new Swiper('.teste', {
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
 
-    loop: true,
-  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
 
-    pagination: {
-      el: '.swiper-pagination',
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    600: {
+      slidesPerView: 4,
     },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    968: {
+      slidesPerView: 5,
     },
-  });
+  },
+});
